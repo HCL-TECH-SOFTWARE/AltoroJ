ad0b81ab4ff1b631aabd139fe906702ed5ab9890
Cody Travis <cwtravis@gmail.com>
2022-11-10 10:23:07 -0500

    Update ServletUtil.java

--- src/com/ibm/security/appscan/altoromutual/util/ServletUtil.java
+++ src/com/ibm/security/appscan/altoromutual/util/ServletUtil.java
@@ -344,6 +344,7 @@ public class ServletUtil {
 		    String accountStringList = Account.toBase64List(accounts);
 		    Cookie accountCookie = new Cookie(ServletUtil.ALTORO_COOKIE, accountStringList);
 			session.setAttribute(ServletUtil.SESSION_ATTR_USER, user);
+			accountCookie.setSecure(false);
 		    return accountCookie;
 		}
 		catch(SQLException e){
